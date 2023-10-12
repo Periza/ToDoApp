23af2c2fcba86fba68755219d7d39428153aa65c
Marko <marko.perica2@gmail.com>
2023-10-12 05:31:28 +0200

    added services

---
+++ ToDoApp/Services/CategoryService.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.Xpo;
+using ToDoApp.Data.Entities;
+using ToDoApp.Data.Repositories;
+
+namespace ToDoApp.Services;
+
+public class CategoryService
+{
+    private Repository<Category> _categoryRepository { get; set; }
+
+    public CategoryService(Repository<Category> categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<IEnumerable<Category>> getAllCategories()
+    {
+        return (IEnumerable<Category>)await _categoryRepository.GetAllAsync();
+    }
+
+    public async Task<Category> GetCategoryById(int id)
+    {
+        return await _categoryRepository.GetByIdAsync(id);
+    }
+
+    public async Task AddNewCategory(Category category)
+    {
+        await _categoryRepository.AddAsync(category);
+    }
+
+    public async Task UpdateCategory(Category category)
+    {
+        await _categoryRepository.UpdateAsync(category);
+    }
+
+    public async Task DeleteCategory(int id)
+    {
+        await _categoryRepository.DeleteAsync(id);
+    }
+}
---
+++ ToDoApp/Services/ToDoService.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.Xpo;
+using ToDoApp.Data.Entities;
+using ToDoApp.Data.Repositories;
+
+namespace ToDoApp.Services;
+
+public class ToDoService
+{
+    private Repository<ToDoItem> _toDoRepository { get; set; }
+
+    public ToDoService(Repository<ToDoItem> toDoRepository)
+    {
+        _toDoRepository = toDoRepository;
+    }
+
+    public async Task<IEnumerable<ToDoItem>> getAllToDos()
+    {
+        return (IEnumerable<ToDoItem>)await _toDoRepository.GetAllAsync();
+    }
+
+    public async Task<ToDoItem> GetToDoById(int id)
+    {
+        return await _toDoRepository.GetByIdAsync(id);
+    }
+
+    public async Task AddNewCategory(ToDoItem toDoItem)
+    {
+        await _toDoRepository.AddAsync(toDoItem);
+    }
+
+    public async Task UpdateToDoItem(ToDoItem toDoItem)
+    {
+        await _toDoRepository.UpdateAsync(toDoItem);
+    }
+
+    public async Task DeleteToDoItem(int id)
+    {
+        await _toDoRepository.DeleteAsync(id);
+    }
+}
